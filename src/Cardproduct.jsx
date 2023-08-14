b5b18a3cd12b7e71e7e00306e8e5bd80226f2478
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2023-08-14 17:41:20 +0300

    create new pr

---
+++ src/Cardproduct.jsx
@@ -0,0 +1,15 @@
+import { Fragment, useState } from 'react'
+
+
+const products = [
+    {}
+]
+export default function CardProd(){
+    return(
+        <>
+        <div className="border-2 border-orange-200 w-1/4 m-auto">
+            <h1>Santi</h1>
+        </div>
+        </>
+    )
+}
